a7e01c294d846008c2428916a22e1e4027616825
Rahul Suthar <rahulsuthar681@gmail.com>
2022-12-19 20:58:39 +0530

    uploading dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftNumber = 1;
+  int RightNumber = 1;
+
+  void changeDice(){
+    setState(() {
+      leftNumber = Random().nextInt(6)+1;
+      RightNumber = Random().nextInt(6)+1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                child: Image(
+                  image: AssetImage('images/dice$leftNumber.png'),
+                ),
+                onPressed: () {
+                  changeDice();
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                child: Image.asset('images/dice$RightNumber.png'),
+                onPressed: () {
+                  changeDice();
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
